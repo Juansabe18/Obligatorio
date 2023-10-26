7273683fd94b32baed563f94712f1296e5ce1eb2
Juan Bellido <jbellido11cb@gmail.com>
2023-10-25 23:19:56 -0300

    se añaden algunos métodos para seguir trabajando

---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+let userLogged;
+let system = new System();
+
+window.addEventListener("load", loadListeners);
+
+function loadListeners() {
+
+}
+
+function addUser () {
+    
+}
+
+function userValidations() {
+
+}
+
+function getUser() {
+
+}
+
+function login() {
+
+}
+
+function hideThings() {
+
+}
+
+function showThings() {
+
+}
+
+
